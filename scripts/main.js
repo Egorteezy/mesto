8f564467a6a0df21cba82a2d062662b166e06867
Egorteezy <egorteezy@gmail.com>
2021-03-01 16:15:56 +0300

    'second-try'

--- scripts/main.js
+++ scripts/main.js
@@ -1,97 +1,70 @@
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
-  let container = document.querySelector('.elements');
-  let placeCardTemplate = container.querySelector('#placeCard');
-  let placeCard = placeCardTemplate.content;
+  const container = document.querySelector('.elements');
+  const placeCardTemplate = container.querySelector('#placeCard');
+  const placeCard = placeCardTemplate.content;
   
-  for(let i=0; i < initialCards.length; i++) {
-    let newPlace = placeCard.querySelector('.element').cloneNode(true)
-    let placeTitle = newPlace.querySelector('.element__title')
-    let placePhoto = newPlace.querySelector('.element__photo')
-    placeTitle.textContent = initialCards[i].name
-    placePhoto.src = initialCards[i].link
+  function createCard() {
+    const newPlace = placeCard.querySelector('.element').cloneNode(true)
+    const placeTitle = newPlace.querySelector('.element__title')
+    const placePhoto = newPlace.querySelector('.element__photo')
+    placeTitle.textContent = nameOfPlace
+    placePhoto.src = linkOfPlace
+    placePhoto.alt = nameOfPlace
     container.prepend(newPlace)
+    addActives()
   }
-  addActives()
+
+  for(let i=0; i < initialCards.length; i++) {
+    nameOfPlace = initialCards[i].name
+    linkOfPlace = initialCards[i].link
+    createCard()
+  }
   
-  const popup = document.querySelector('.popup');
-  const popupOpenButton = document.querySelector('.profile__edit');
-  const popupCloseButton = document.querySelector('.popup__close');
-  const formElement = popup.querySelector('.popup__container');
-  let nameInput = formElement.querySelector('#input_name')
-  let jobInput = formElement.querySelector('#input_prof')
+  const popupProfile = document.querySelector('.popup_type_profile');
+  const profileOpenButton = document.querySelector('.profile__edit');
+  const profileCloseButton = document.querySelector('.popup__close_type_profile');
+  const nameInput = document.querySelector('.popup__input_type_name')
+  const jobInput = document.querySelector('.popup__input_type_profession')
   const authorName = document.querySelector('.profile__name');
   const authorProf = document.querySelector('.profile__profession');
-  const popupAdd = document.querySelector('.popup-add');
+  const popupAdd = document.querySelector('.popup_type_card-add');
   const addPlaceButton = document.querySelector('.profile__add-button');
-  const popupAddCloseButton = document.querySelector('.popup-add__close');
-  const popupAddSave = document.querySelector('.popup-add__save');
-  const titleInput = popupAdd.querySelector('#input_title');
-  const photoInput = popupAdd.querySelector('#input_picture');
+  const popupAddCloseButton = document.querySelector('.popup__close_type__card-add');
+  const titleInput = popupAdd.querySelector('.popup__input_type_title');
+  const photoInput = popupAdd.querySelector('.popup__input_type_photo');
   const deleteButton = document.querySelector('.element__delete');
-  const popupPic = document.querySelector('.popup-pic')
-  const popupPicCloseButton = document.querySelector('.popup-pic__close')
+  const popupPic = document.querySelector('.popup_type_image')
+  const popupPicCloseButton = document.querySelector('.popup__close_type_image')
+  const popupImage = document.querySelector('.popup__image')
   
-  function openPopup() {
-      popup.classList.add('popup_opened')
-      nameInput.value = authorName.textContent
-      jobInput.value = authorProf.textContent
+  function openPopup(popup) {
+    popup.classList.add('popup_opened')
   }
-  
-  function closePopup() {
-      popup.classList.remove('popup_opened')
+
+  function closePopup(popup) {
+    popup.classList.remove('popup_opened')
   }
-  
-  function formSubmitHandler (evt) {
-      evt.preventDefault(); 
-      authorName.textContent = nameInput.value
-      authorProf.textContent = jobInput.value
-      closePopup()
+
+  function openPopupProfile() {
+    nameInput.value = authorName.textContent
+    jobInput.value = authorProf.textContent
+    openPopup(popupProfile)
   }
-  
-  function openPopupAdd() {
-      popupAdd.classList.add('popup-add_opened')
-  }
-  
-  function closePopupAdd() {
-      popupAdd.classList.remove('popup-add_opened')
+
+  function putNewProfile (evt) {
+    evt.preventDefault(); 
+    authorName.textContent = nameInput.value
+    authorProf.textContent = jobInput.value
+    closePopup(popupProfile)
   }
   
   function addContaner(evt) {
-          evt.preventDefault();
-          let newPlace = placeCard.querySelector('.element').cloneNode(true)
-          let placeTitle = newPlace.querySelector('.element__title')
-          let placePhoto = newPlace.querySelector('.element__photo')
-          placeTitle.textContent = titleInput.value
-          placePhoto.src = photoInput.value
-          placePhoto.alt = titleInput.value
-          container.prepend(newPlace)
-          closePopupAdd()
-          addActives()
+    evt.preventDefault();
+    linkOfPlace = photoInput.value
+    nameOfPlace = titleInput.value
+    createCard()
+    closePopup(popupAdd)
+    photoInput.value = ''
+    titleInput.value = ''
   }
   
   function addActives() {
@@ -105,7 +78,7 @@ const initialCards = [
   
     let activeLikes = document.querySelectorAll('.element__like') 
       for(let i=0; i < activeLikes.length; i++) { 
-          activeLikes[i].addEventListener('click', like) 
+          activeLikes[i].addEventListener('click', like)
         } 
     
     let openPic = document.querySelectorAll('.element__photo')
@@ -119,23 +92,18 @@ const initialCards = [
   }
   
   function openPopupPic(evt) {
-    let popupImage = document.querySelector('.popup-pic__image')
     popupImage.src = evt.target.src 
-    popupPic.classList.add('popup-pic_opened')
+    openPopup(popupPic)
     let cardTitle = evt.target.closest('.element').querySelector('.element__title').textContent
-    document.querySelector('.popup-pic__title').textContent = cardTitle
+    document.querySelector('.popup__title_type_image').textContent = cardTitle
     popupImage.alt = cardTitle
   }
   
-  function closePopupPic() {
-    popupPic.classList.remove('popup-pic_opened')
-  }
-  
-  
-  popupOpenButton.addEventListener('click', openPopup)
-  popupCloseButton.addEventListener('click', closePopup)
-  formElement.addEventListener('submit', formSubmitHandler)
-  addPlaceButton.addEventListener('click', openPopupAdd)
-  popupAddCloseButton.addEventListener('click', closePopupAdd)
-  popupAddSave.addEventListener('click', addContaner)
-  popupPicCloseButton.addEventListener('click', closePopupPic)
+  profileOpenButton.addEventListener('click', openPopupProfile)
+  profileCloseButton.addEventListener('click',  () => {closePopup(popupProfile)})
+  popupProfile.addEventListener('submit', putNewProfile)
+  addPlaceButton.addEventListener('click', () => {openPopup(popupAdd)})
+  popupAddCloseButton.addEventListener('click', () => {closePopup(popupAdd)})
+  popupAdd.addEventListener('submit', addContaner)
+  popupPicCloseButton.addEventListener('click', () => {closePopup(popupPic)})
+
